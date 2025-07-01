5d0b75a1cf9aaf7146d62ec6ee2d3380b39fa25e
jdwolf <jdwolf2@gmail.com>
2025-07-01 18:47:43 -0400

    Clean: remove locked node_modules and reinstall for Netlify

--- vue.config.js
+++ vue.config.js
@@ -1,39 +1,10 @@
-// vue.config.js
-// const { defineConfig } = require('@vue/cli-service')
-// const fs = require('fs')
-
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-//   devServer: {
-//     host: 'localhost',
-//     port: 5173,
-//     https: {
-//       key: fs.readFileSync('./certs/localhost-key.pem'),
-//       cert: fs.readFileSync('./certs/localhost.pem'),
-//     },
-//     allowedHosts: 'all',
-//   },
-// })
-
-// module.exports = {
-//   pwa: {
-//     name: 'AccuSalt Mobile',
-//     themeColor: '#35b86b',
-//     msTileColor: '#ffffff',
-//     appleMobileWebAppCapable: 'yes',
-//     appleMobileWebAppStatusBarStyle: 'black',
-
-//     workboxOptions: {
-//       skipWaiting: true,
-//       clientsClaim: true,
-//     },
-//   },
-// }
-pwa: {
-  name: 'AccuSalt',
-  themeColor: '#000000',
-  manifestOptions: {
-    start_url: '/',
-    display: 'standalone'
-  }
+module.exports = {
+  pwa: {
+    name: 'AccuSalt Mobile',
+    themeColor: '#000000',
+    manifestOptions: {
+      start_url: '/',
+      display: 'standalone',
+    },
+  },
 }
