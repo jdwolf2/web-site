094d168046ccaba43c4c38cd0fef45fda8254b96
jdwolf2 <jdwolf200@gmail.com>
2025-07-02 15:53:40 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import { registerLicense } from '@syncfusion/ej2-base'
-import './registerServiceWorker'
 registerLicense(
   'Ngo9BigBOggjHTQxAR8/V1NNaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXlfcnZURGZfV0V2X0dWYUA='
 )
