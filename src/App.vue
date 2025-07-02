2bd106f43452021abec2bb1b1347729489056b07
jdwolf2 <jdwolf200@gmail.com>
2025-07-02 18:50:24 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -349,6 +349,15 @@ function onExportToExcel() {
   margin: 20px;
   color: #333;
 }
+
+  <style>
+  @import '../node_modules/@syncfusion/ej2-base/styles/material.css';
+  @import '../node_modules/@syncfusion/ej2-buttons/styles/material.css';
+  @import '../node_modules/@syncfusion/ej2-inputs/styles/material.css';
+  @import '../node_modules/@syncfusion/ej2-popups/styles/material.css';
+  @import '../node_modules/@syncfusion/ej2-lists/styles/material.css';
+  @import "../node_modules/@syncfusion/ej2-vue-calendars/styles/material.css";
+</style>
 </style>
 
 
