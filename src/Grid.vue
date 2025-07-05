3ce63a18e4c43eeaba9b07f6b4e11ba19272d1ac
jdwolf2 <jdwolf200@gmail.com>
2025-07-05 13:05:21 -0400

    Update Grid.vue

--- src/Grid.vue
+++ src/Grid.vue
@@ -105,7 +105,7 @@ const onHeaderCellInfo = (args) => {
   headerCell.style.padding = '0 5px'
   headerCell.style.borderRight = '1px solid white'
   headerCell.style.color = 'white'
-  headerCell.style.height = '40px'
+  headerCell.style.height = '50px'
   switch (columnField) {
     case 'siteName':
     case 'Truck':
@@ -387,5 +387,9 @@ body {
   color: black;
   background-color: yellow;
   border: 7px solid yellow;
+}
+
+.e-columnheader{
+  height: 50px !important;
 }
 </style>
