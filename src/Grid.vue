7413fa078c508de75ff7bf6ce54491999a4f43bd
jdwolf2 <jdwolf200@gmail.com>
2025-07-05 16:08:45 -0400

    Update Grid.vue

--- src/Grid.vue
+++ src/Grid.vue
@@ -132,7 +132,7 @@ const onHeaderCellInfo = (args) => {
   }
 }
 </script>
-<style>
+<style scoped>
 @import '~@syncfusion/ej2-base/styles/material.css';
 @import '~@syncfusion/ej2-buttons/styles/material.css';
 @import '~@syncfusion/ej2-calendars/styles/material.css';
