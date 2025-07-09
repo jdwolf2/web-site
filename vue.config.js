26fa9bb3df291224fd56bc0e2d5ccc6f321dade7
jdwolf <jdwolf2@gmail.com>
2025-07-08 23:41:50 -0400

    working on mobil modification

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'AccuSalt Mobile',
+    themeColor: '#2196f3',
+    background_color: '#ffffff',
+    display: 'standalone',
+    start_url: '.',
+    orientation: 'portrait',
+    manifestOptions: {
+      short_name: 'AccuSalt',
+    },
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true,
+    },
+  },
 })
